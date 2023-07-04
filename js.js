d44769d7d62da4ee50924b2ae416f86bdf80b24f
alphin <alphinpoly8@gmail.com>
2023-07-04 22:45:08 +0530

    onclick functions for clear and delete created

--- js.js
+++ js.js
@@ -13,20 +13,55 @@ function div(a, b){
 
 var currentValue;
 var preValue;
+var opValue;
+
+curScreen = document.getElementById("curScreen");
+preScreen = document.getElementById("preScreen");
 
 function keyPressed(n){
-    currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+n;
+    
+    curScreen.value = curScreen.value+n;
 }
 
 function opPressed(op){
+    
+    preValue = curScreen.value;
+    preScreen.value = preScreen.value + curScreen.value;
+    curScreen.value = "";
     if(op == '+'){
-        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'+';
-        
+        curScreen.value = curScreen.value+'+';
+        opValue = 1;
     }else if(op == '-'){
-        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'-';
+        curScreen.value = curScreen.value+'-';
+        opValue = 2;
     }else if(op == '*'){
-        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'*';
+        curScreen.value = curScreen.value+'*';
+        opValue = 3;
     }else if(op == '/'){
-        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'/';
+        curScreen.value = curScreen.value+'/';
+        opValue = 4;
     }
+}
+
+function allClear(){
+    curScreen.value = "";
+    preScreen.value = "";
+}
+function lastClear(){
+    var now = curScreen.value;
+    now = Math.floor(now/10);
+    if(now == 0){
+        curScreen.value = "";
+    }else{
+        curScreen.value = now;
+    }
+}
+
+function eqPressed(){
+    currentValue = curScreen.value;
+    curScreen.value = "";
+    if(opValue == 1){
+        curScreen.value = Math.praseInt(add(preValue, currentValue));
+    }
+    curScreen.value = preValue;
 }
