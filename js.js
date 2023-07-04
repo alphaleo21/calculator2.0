0e519a69a0fcd547d8c7f8c922b68d0b78732e45
alphin <alphinpoly8@gmail.com>
2023-07-04 22:00:57 +0530

    function for onclick for number and operator buttons

--- js.js
+++ js.js
@@ -11,3 +11,22 @@ function div(a, b){
     return a / b;
 }
 
+var currentValue;
+var preValue;
+
+function keyPressed(n){
+    currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+n;
+}
+
+function opPressed(op){
+    if(op == '+'){
+        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'+';
+        
+    }else if(op == '-'){
+        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'-';
+    }else if(op == '*'){
+        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'*';
+    }else if(op == '/'){
+        currentValue = document.getElementById("curScreen").value = document.getElementById("curScreen").value+'/';
+    }
+}
