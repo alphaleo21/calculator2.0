edaceff9d33f8f8a978ee40c7391153cb9148667
alphin <alphinpoly8@gmail.com>
2024-03-01 12:01:53 +0530

    now the calculator can calculate multiple operands at once

--- js.js
+++ js.js
@@ -3,13 +3,20 @@ var preValue;
 var curValue;
 var op;
 var result;
+var operandCount;
 
 function keyPressed(e){
+    // if(op != null){
+    //     eqPressed();
+    // }
     screen.value = screen.value+e;
 }
 
 function allClear(){
     screen.value = '';
+    preValue = 0;
+    curValue = 0;
+    operandCount = 0;
 }
 
 function lastClear(){
@@ -17,28 +24,36 @@ function lastClear(){
 }
 
 function opPressed(e){
-    preValue = +(screen.value);
-    screen.value = '';
-    switch(e){
-        case '+':
-            op = 1;
-            break;
-        case '-':
-            op = 2;
-            break;
-        case '/':
-            op = 3;
-            break;
-        case '*':
-            op = 4;
-            break;
-        default:
-            break;
+    if(operandCount == 1){
+      performOperation();
+      screen.value = preValue;
+      console.log(preValue);
+    }else{
+      operandCount = 1;
     }
+    switch (e) {
+      case "+":
+        op = 1;
+        break;
+      case "-":
+        op = 2;
+        break;
+      case "/":
+        op = 3;
+        break;
+      case "*":
+        op = 4;
+        break;
+      default:
+        break;
+    }
+    preValue = +screen.value;
+    
+    screen.value = "";
 }
 
-function eqPressed() {
-    curValue = +(screen.value);
+function performOperation(){
+    curValue = +screen.value;
     switch (op) {
       case 1:
         result = preValue + curValue;
@@ -55,6 +70,28 @@ function eqPressed() {
       default:
         break;
     }
+    preValue = result;
+}
+
+function eqPressed() {
+    curValue = +screen.value;
+    switch (op) {
+      case 1:
+        result = preValue + curValue;
+        break;
+      case 2:
+        result = preValue - curValue;
+        break;
+      case 3:
+        result = preValue / curValue;
+        break;
+      case 4:
+        result = preValue * curValue;
+        break;
+      default:
+        break;
+    }
+    operandCount = 0;
     screen.value = '';
     screen.value = result;
 }
